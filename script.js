f56598c76c7047fd1cf2444b8b1e36263ee3ca77
Olegs Celinskis <olegs.celinskis@towerswatson.com>
2021-10-15 22:12:09 +0100

    commit

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-const NotionPageToHtml = require('notion-page-to-html');
+const notionDocId = "0cb628857f3c4c77bf7f9a879a6ec21d"
 
-// using then
-NotionPageToHtml.convert("https://www.notion.so/asnunes/Simple-Page-Text-4d64bbc0634d4758befa85c5a3a6c22f").then((page) => console.log(page));
-
-// using async/await
-async function getPage() {
-  const { title, icon, cover, html } = await NotionPageToHtml.convert("https://www.notion.so/asnunes/Simple-Page-Text-4d64bbc0634d4758befa85c5a3a6c22f");
-  console.log(title, icon, cover, html);
-}
-
-getPage();
+fetch("https://potion-api.now.sh/html?id=" + notionDocId)
+  .then(res => res.text())
+  .then(text => {
+    document.querySelector("main").innerHTML = text
+  })
