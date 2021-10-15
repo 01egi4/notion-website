f13d3912fbb6233e0727210092f7506045e87278
Olegs Celinskis <olegs.celinskis@towerswatson.com>
2021-10-15 22:32:57 +0100

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const notionDocId = "4d64bbc0634d4758befa85c5a3a6c22f"
 
-fetch("https://www.notion.so/asnunes/Simple-Page-Text-2-" + notionDocId)
+fetch("https://notion-page-to-html-api.vercel.app/html?id=" + notionDocId)
   .then(res => res.text())
   .then(text => {
     document.querySelector("main").innerHTML = text
